9bbc4b0bc78cc6e919e4e3d7dcfd8d7f013034c3
Alex Parinov <creafz@gmail.com>
2020-11-25 13:33:00 +0300

    Use buffer size 1

--- pyv4l2/frame.pyx
+++ pyv4l2/frame.pyx
@@ -39,7 +39,7 @@ cdef class Frame:
 
 
         memset(&self.buf_req, 0, sizeof(self.buf_req))
-        self.buf_req.count = 4
+        self.buf_req.count = 1
         self.buf_req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE
         self.buf_req.memory = V4L2_MEMORY_MMAP
 
